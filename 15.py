8f46f7622b39a29b4e857ec95703cf36ec10ac1c
Ahmed <ahmadasaadmajed1@gmail.com>
2024-10-07 09:37:05 +0300

    Message Text Types handlers

---
+++ 15.py
@@ -0,0 +1,37 @@
+import telebot
+from telebot.util import content_type_media
+token = "7515698207:AAH8vxp3bTLzpkMfb4g_l0cuURiQ4cEbA2Q"
+bot = telebot.TeleBot(token)
+
+
+# Message Text Type
+    # 1- Commmand
+    # 2- Status
+    # 3- Any Text
+
+
+
+@bot.message_handler(commands=['start', 'help', 'id'])
+def handler1(msg):
+    bot.send_message(msg.chat.id, "انت ارسلت رسالة امرية")
+
+
+
+@bot.message_handler(regexp=r'age = [0-9]{1,3}$')
+def handler2(msg):
+    bot.send_message(msg.chat.id, "انت ادخلت تعبيرا")
+
+
+
+@bot.message_handler(content_types=['text'])
+def handler3(msg):
+    bot.send_message(msg.chat.id, "مرحبا شكرا لارسال الرسالة.")
+
+
+# @bot.message_handler(content_types=['text'])
+# def handler4(msg):
+#     bot.send_message(msg.chat.id, "مرحبا شكرا لارسال msg.")
+
+
+print("Bot is Running")
+bot.polling()
